0d18ec8e8ef4ae6a6025061e8edff69e32a95092
EzfenCheung <ezfen_zhang@163.com>
2016-05-16 17:16:01 +0800

    添加测试代码

--- EZQRCodeScanner/ViewController.m
+++ EZQRCodeScanner/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "EZQRCodeScannerView.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    EZQRCodeScannerView *testView = [[EZQRCodeScannerView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:testView];
+//    [testView startRunning];
 }
 
 - (void)didReceiveMemoryWarning {
