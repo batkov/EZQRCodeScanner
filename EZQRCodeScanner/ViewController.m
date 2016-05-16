d4e5ac4f652ca5726120a441a0fb3674986e5fe0
EzfenCheung <ezfen_zhang@163.com>
2016-05-16 16:36:23 +0800

    initial commit

---
+++ EZQRCodeScanner/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EZQRCodeScanner
+//
+//  Created by ezfen on 16/5/16.
+//  Copyright © 2016年 Ezfen Cheung. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
